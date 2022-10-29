ac4d7a81f6cfa981f9f19ef157e2e8cd8bd047b6
jim <326695231@qq.com>
2022-10-29 22:39:42 +0800

    update blog config

---
+++ docs/.vuepress/config/head.ts
@@ -0,0 +1,24 @@
+import { VdoingThemeConfig } from 'vuepress-theme-vdoing/types'
+
+const head: VdoingThemeConfig['head'] = [
+    ['link', { rel: 'icon', href: '/img/favicon.ico' }], //favicons，资源放在public文件夹
+    [
+      'meta',
+      {
+        name: 'keywords',
+        content: '后端博客,个人技术博客,后端,后端开发,后端框架,后端面试题,技术文档,学习,面试,python,go,redis,k8s,mysql,kafka,flask,django,tornado,git,github,markdown',
+      },
+    ],
+    ['meta', { name: 'baidu-site-verification', content: '7F55weZDDc' }], // 百度统计的站长验证（你可以去掉）
+    ['meta', { name: 'theme-color', content: '#11a8cd' }], // 移动浏览器主题颜色
+    // [
+    //   'script',
+    //   {
+    //     'data-ad-client': 'ca-pub-7828333725993554',
+    //     async: 'async',
+    //     src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js',
+    //   },
+    // ], // 网站关联Google AdSense 与 html格式广告支持（你可以去掉）
+]
+
+export default head
