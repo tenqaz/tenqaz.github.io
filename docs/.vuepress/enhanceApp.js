3385fc13037ccad5f5ca394c3c8a4f2e7333fac9
jim <326695231@qq.com>
2022-10-30 16:33:01 +0800

    update

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -6,13 +6,13 @@ export default ({
   siteData // 站点元数据
 }) => {
   // window.Vue = vue // 使页面中可以使用Vue构造函数 （使页面中的vue demo生效）
-  router.beforeEach((to, from, next) => {
-    if (typeof _hmt !== "undefined") {
-      if (to.path) {
-        _hmt.push(["_trackPageview", to.fullPath]);
-      }
-    }
+  // router.beforeEach((to, from, next) => {
+  //   if (typeof _hmt !== "undefined") {
+  //     if (to.path) {
+  //       _hmt.push(["_trackPageview", to.fullPath]);
+  //     }
+  //   }
     
-    next();
-  });
+  //   next();
+  // });
 }
