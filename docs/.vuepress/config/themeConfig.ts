b9f202a3c5761d5642aaf316f565292ff2971de5
zhengwenfeng <326695231@qq.com>
2023-01-06 10:14:12 +0800

    update

--- docs/.vuepress/config/themeConfig.ts
+++ docs/.vuepress/config/themeConfig.ts
@@ -88,7 +88,7 @@ const themeConfig: VdoingThemeConfig['themeConfig'] = {
           { text: '归档', link: '/archives/' },
         ],
       },
-      { text: '开往', link: 'https://travellings.link' }
+      { text: '开往', link: 'https://www.travellings.cn/go.html' }
     ],
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
     logo: '/img/me.jpg', // 导航栏logo
